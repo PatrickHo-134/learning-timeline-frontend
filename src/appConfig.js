bbe56f5c3dd426028ef479d13bfeb54e7ac077cf
Patrick Ho <patrick@blueridgeglobal.com.au>
2024-09-28 23:12:52 +1000

    #34 Add signature and version number

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,1 +1,2 @@
+export const appVersion = '1.0.0'
 export const apiBaseUrl = process.env.REACT_APP_API_URL;
--- src/components/CollectionList.js
+++ src/components/CollectionList.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import {
+  Box,
   IconButton,
   List,
   ListItem,
   ListItemText,
   Tooltip,
+  Typography,
 } from "@mui/material";
 import ArchiveIcon from "@mui/icons-material/Archive";
 import { useDispatch, useSelector } from "react-redux";
@@ -12,10 +14,10 @@ import {
   fetchCollections,
   archiveCollection,
 } from "../actions/collectionActions";
-import { Container } from "react-bootstrap";
 import AddCollectionForm from "./AddCollectionForm";
 import { fetchLearningNotes } from "../actions/learningNoteActions";
 import { setCategoryFilter } from "../actions/pageFilterActions";
+import { appVersion } from "../appConfig";
 
 const CollectionList = () => {
   const dispatch = useDispatch();
@@ -50,7 +52,9 @@ const CollectionList = () => {
   useEffect(() => {
     if (!loading && collections.length > 0) {
       setSelectedCollectionId(0);
-      dispatch(fetchLearningNotes({ collection_id: 0, labels: [], userInfo: userInfo })); // FIXME: update labels list
+      dispatch(
+        fetchLearningNotes({ collection_id: 0, labels: [], userInfo: userInfo })
+      ); // FIXME: update labels list
     }
   }, [loading, collections, userInfo, dispatch]);
 
@@ -63,49 +67,75 @@ const CollectionList = () => {
   }
 
   return (
-    <Container>
+    <Box
+      sx={{
+        position: "relative",
+        height: "100%",
+      }}
+    >
       <h3>Categories</h3>
-      {Array.isArray(collections) && collections.length > 0 ? (
-        <List>
-          {collections.map((collection, index) => (
-            <ListItem
-              key={index}
-              onMouseEnter={() => setHoveredCollectionId(collection.id)}
-              onMouseLeave={() => setHoveredCollectionId(null)}
-              onClick={() => handleCollectionSelect(collection.id)}
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-              style={{
-                backgroundColor:
-                  selectedCollectionId === collection.id
-                    ? "#b3cde0"
-                    : "#ffffff",
-              }}
-            >
-              <ListItemText primary={collection.name} />
 
-              {collection.id !== 0 && hoveredCollectionId === collection.id && (
-                <Tooltip title="Archive Collection">
-                  <IconButton
-                    edge="end"
-                    color="primary"
-                    onClick={() => handleArchiveCollection(collection.id)}
-                  >
-                    <ArchiveIcon />
-                  </IconButton>
-                </Tooltip>
-              )}
-            </ListItem>
-          ))}
-        </List>
-      ) : (
-        <div></div>
-      )}
-      <AddCollectionForm />
-    </Container>
+      <Box sx={{ flexGrow: 1 }}>
+        {Array.isArray(collections) && collections.length > 0 ? (
+          <List>
+            {collections.map((collection, index) => (
+              <ListItem
+                key={index}
+                onMouseEnter={() => setHoveredCollectionId(collection.id)}
+                onMouseLeave={() => setHoveredCollectionId(null)}
+                onClick={() => handleCollectionSelect(collection.id)}
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+                style={{
+                  backgroundColor:
+                    selectedCollectionId === collection.id
+                      ? "#b3cde0"
+                      : "#ffffff",
+                }}
+              >
+                <ListItemText primary={collection.name} />
+
+                {collection.id !== 0 &&
+                  hoveredCollectionId === collection.id && (
+                    <Tooltip title="Archive Collection">
+                      <IconButton
+                        edge="end"
+                        color="primary"
+                        onClick={() => handleArchiveCollection(collection.id)}
+                      >
+                        <ArchiveIcon />
+                      </IconButton>
+                    </Tooltip>
+                  )}
+              </ListItem>
+            ))}
+          </List>
+        ) : (
+          <div></div>
+        )}
+        <AddCollectionForm />
+      </Box>
+
+      <Box
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: 20,
+          width: "15%",
+          padding: "1rem",
+          textAlign: "center",
+          backgroundColor: "#f5f5f5",
+          borderTop: "1px solid #ddd",
+          boxSizing: "border-box",
+        }}
+      >
+        <Typography variant="body2">Created by Patrick Ho</Typography>
+        <Typography variant="caption">Version: {appVersion}</Typography>
+      </Box>
+    </Box>
   );
 };
 
