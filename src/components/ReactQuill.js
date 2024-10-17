7a156ea7438a09d5c166c8214ddcedf693ac4449
Patrick Ho <patrick@blueridgeglobal.com.au>
2024-10-18 01:42:37 +1100

    #51 Fix issue with react-quill style

--- src/components/ReactQuill.js
+++ src/components/ReactQuill.js
@@ -1,32 +1,45 @@
+import { Box } from "@mui/material";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "../App.css";
+import '../custom-quill-styles.css';
 
-export const AutoHeightQuill = ({ content }) => {
+export const ReadOnlyContent = ({ content }) => {
   return (
-    <div style={{ height: 'auto' }}
+    <Box component="section" sx={{
+      bgcolor: "background.paper",
+      margin: 1,
+    }}
     >
       <ReactQuill
         value={content}
-        readOnly // Set the readOnly prop to make the content read-only
+        readOnly
         modules={{
-          toolbar: false, // Set the toolbar to false to remove it
+          // use matchVisual config to strip out the unnecessarily-injected <p><br></p>
+          clipboard: {
+            matchVisual: false
+          },
+          toolbar: false,
         }}
       />
-    </div>
+    </Box>
   );
 };
 
 const modules = {
+  // use matchVisual config to strip out the unnecessarily-injected <p><br></p>
+  clipboard: {
+    matchVisual: false
+  },
   toolbar: [
     [{header:1}, {header:2}],
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
     [{ font: [] }, { size: ["small", false, "large", "huge"] }],
-    ["bold", "italic", "underline", "strike", "code"], // toggled buttons
+    ["bold", "italic", "underline", "strike", "code"],
     [{ align: [] }, { list: "ordered" }, { list: "bullet" }, { indent: "-1" }, { indent: "+1" }, { direction: "rtl" }],
     ["blockquote", "code-block"],
-    [{ script: "sub" }, { script: "super" }], // superscript/subscript
-    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ script: "sub" }, { script: "super" }],
+    [{ color: [] }, { background: [] }],
     ["link", "image", "video"],
     ["clean"], // remove formatting button
   ],
