3f43c3f3343789a4044467252dbf91e01c970117
Patrick Ho <patrick@blueridgeglobal.com.au>
2024-10-18 22:55:00 +1100

    #51 Update react-quill component style

--- src/components/EditLearningNoteModal.js
+++ src/components/EditLearningNoteModal.js
@@ -29,7 +29,7 @@ const EditLearningNoteModal = ({ learningNote, onClose }) => {
           bgcolor: "background.paper",
           p: 3,
           borderRadius: 2,
-          minWidth: "70%",
+          minWidth: "80%",
           maxHeight: "90%",
         }}
       >
